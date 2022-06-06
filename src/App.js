7c14cdd09d4788b7714053eb8a635913ee286cd2
Jonibek <94430009+MJony05@users.noreply.github.com>
2022-06-07 00:38:21 +0500

    layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+import Main from "./layout/Main";
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </>
   );
 }
 
